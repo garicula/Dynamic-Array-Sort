3ac05325b7451f10c4c624bb5d41dcf17b10f58b
Garrick Morley <garrickmorley15@gmail.com>
2020-11-28 23:49:02 -0500

    Add files via upload

---
+++ Syntax (Dynamic Array).cpp
@@ -0,0 +1,81 @@
+// Garrick Morley
+// CPSC 200 Project 5 B
+// This program outputs numbers from a file in ascending order using a dynamic array
+
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <cmath>
+#include <fstream>
+#include <cassert>
+using namespace std;
+
+// Sorts array[0..n-1] into ascending order
+void sort (double array[], int n)
+{
+int i, j;
+double tmp;
+
+for (i=0; i<n-1; ++i)
+    for (j=i+1; j<n; ++j)
+        if (array[i] > array[j])
+        {
+            tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+        }
+}
+
+main()
+{
+	double score;
+	int i = 0;
+	int a = 0;
+	int size = 0;
+
+	//open the file
+	ifstream input;
+	input.open("data");
+	
+	//make sure it opened correctly
+	if (input.fail())
+	cout << "File did not open correctly" << endl;	
+	
+	//get the count (first line)
+	while ((input >> score) && ( i < 0))
+	{
+		i++;
+	}
+	
+	size = score;
+	
+	//create dynamic array
+	typedef double* doublePtr;
+	doublePtr d;
+	d = new double[size];
+	
+	//feed numbers into array
+	while (input >> score)
+	{
+		d[a] = score;
+		a++;
+	}
+	
+	//sort the numbers
+	sort(d, size);
+	
+	for(int alpha = 0; alpha < size; alpha++)
+	{
+		cout << d[alpha] << endl;
+	}
+		
+}
+
+
+
+
+
+
+
+
+
